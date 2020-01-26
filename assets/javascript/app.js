e1fae37e67b4537fcd70703a1c9e151b4530c562
Celeste Lewis <cdlewis42@gmail.com>
2020-01-26 02:37:06 -0500

    Change from Animal Crossing Villagers to Anime since there were not many gifs for villagers.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,9 +1,9 @@
-//On click, pulls data-villager to add to the queryURL
+//On click, pulls data-anime to add to the queryURL
 $("button").on("click", function(){
     //"This" will pull the buttons data that is clicked.
-    var villager = $(this).attr("data-villager");
+    var anime = $(this).attr("data-anime");
     var apiKey = "&api_key=KFEVEi0ML7VBhu75LJ67c8sj0fHZPiee";
-    var queryURL = "https://api.giphy.com/v1/gifs/search?q=animal+crossing+" + villager + apiKey;
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + anime + apiKey;
     // This will ask GIPHY for it's data to related search query
     $.ajax({
         url: queryURL,
@@ -13,14 +13,14 @@ $("button").on("click", function(){
        console.log(results[0])
        //For every result, this will append a div and p tag for every gif called
        for(var i = 0; i < results.length; i++){
-        var villagerDiv = $("<div>")
+        var animeDiv = $("<div>")
         var p = $("<p>")
         p.append(results[i].rating)
-        var villagerImage = $("<img>")
-        villagerImage.attr("src",results[i].images.fixed_height.url)
-        villagerDiv.append(p)
-        villagerDiv.append(villagerImage);
-        $("#gifs").prepend(villagerDiv)
+        var animeImage = $("<img>")
+        animeImage.attr("src",results[i].images.fixed_height.url)
+        animeDiv.append(p)
+        animeDiv.append(animeImage);
+        $("#gifs").prepend(animeDiv)
 
        }
         

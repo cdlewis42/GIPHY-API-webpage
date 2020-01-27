f6358e3c472b9461b2ae6a3efeac6b28573e6418
Celeste Lewis <cdlewis42@gmail.com>
2020-01-27 18:02:17 -0500

    added document ready to on click function to be able to grab data attributes.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -9,13 +9,16 @@ function buttonReady(arr) {
         button.text(arr[i])
         $("#buttons").append(button)
     }
-
+    
 }
 
 
 
 
+
+
 //On click, pulls data-anime to add to the queryURL
+$(document).ready(function () {
 $("button").on("click", function () {
     //"This" will pull the buttons data that is clicked.
     var anime = $(this).attr("data-anime");
@@ -28,19 +31,43 @@ $("button").on("click", function () {
     }).then(function (response) {
         var results = response.data;
         console.log(results[0])
-        //For every result, this will append a div and p tag for every gif called
+        //For every result, this will append a div and p tag for every gif called.
         for (var i = 0; i < results.length; i++) {
             var animeDiv = $("<div>")
             var p = $("<p>")
             p.append(results[i].rating)
             var animeImage = $("<img>")
+            //This is where I am adding states to change from being still to animating.
+            animeImage.addClass("gif")
+            animeImage.attr("data-still", results[i].images.fixed_height_still.url)
+            animeImage.attr("data-animate", results[i].images.fixed_height.url)
+            animeImage.attr("data-state", "still")
             animeImage.attr("src", results[i].images.fixed_height.url)
             animeDiv.append(p)
             animeDiv.append(animeImage);
             $("#gifs").prepend(animeDiv)
         }
-
+        
 
     })
 })
-buttonReady(topics)
+}
+)
+
+//This is where I am creating the on click function to make the images go from still to animating and vice versa.
+    $(".gif").on("click", function () {
+            var state = $(this).attr("data-state");
+            if (state === "still") {
+                $(this).attr("src", $(this).attr("data-animate"));
+                $(this).attr("data-state", "animate");
+            } else {
+                $(this).attr("src", $(this).attr("data-still"))
+                $(this).attr("data-state", "still")
+            }
+
+        })
+        
+        
+        
+        
+        buttonReady(topics)
